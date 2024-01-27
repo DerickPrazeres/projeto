cc08b81f226f41cb0adf6cca1a92857968b279db
Derick Prazeres <deh1202@gmail.com>
2024-01-27 12:20:13 -0300

    Adjusts

--- .vscode/script.js
+++ .vscode/script.js
@@ -2,20 +2,18 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img")
 
- // if(html.classList.contains('light')) {
-    //html.classList.remove('light')
-  //} else {
-   // html.classList.add('light')
-   
-   if(html.classList.contains('light')) {
-// quando estiver em light mode, add a imagem light
+  // if(html.classList.contains('light')) {
+  // html.classList.remove('light')
+  // } else {
+  // html.classList.add('light')
+
+  if (html.classList.contains("light")) {
+    // quando estiver em light mode, add a imagem light
     img.setAttribute("src", "./assets/derick-light.png")
-   } else {
-// quando não em light mode, manter a img normal
+  } else {
+    // quando não em light mode, manter a img normal
     img.setAttribute("src", "./assets/derick.png")
-   }
-
   }
-  
+}
